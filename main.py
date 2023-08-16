82c4ed3b1b250db70e9c2dc2f520d46f036b711f
Konno <teamsakura.act@gmail.com>
2023-08-16 14:49:13 +0900

    CORSエラーを回避する処理を追加

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
+# CORSを回避するために追加
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/root")
 async def root():
     return {"message": "Hello World From Fast API"}
